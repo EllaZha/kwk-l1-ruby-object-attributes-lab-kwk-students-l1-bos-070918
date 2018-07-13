3fcfadd00ce1d13b23f4a5333448e98a95228ed8
Ella Zhang <eyanchizhang@gmail.com>
2018-07-13 18:53:50 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,17 +1,9 @@
 # dog.rb
  
- class dog
-   def name=(name)
-     @name=name
-   end
-   def name
-     @name
-   end
-   def breed=(breed)
-     @breed=breed
-   end
-   def breed
-     @breed
-   end
-  end
-  
+class Dog 
+
+attr_accessor :name, :breed
+
+end
+
+fido
